ecd68b8d56f3b07c9d855f611588779d40776438
Johnny Freire <johnnyfreirejh@hotmail.com>
2021-09-28 02:25:29 -0300

    Feature: Item Home

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -43,14 +43,73 @@ class _HomePageState extends ModularState<HomePage, HomeStore> {
             final list = controller.status.value as List<CharacterModel>;
 
             if (list.isNotEmpty) {
-              return ListView.builder(
+              return GridView.builder(
+                shrinkWrap: true,
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
+                scrollDirection: Axis.vertical,
+                physics: const ScrollPhysics(),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 4,
+                  mainAxisSpacing: 4,
+                  childAspectRatio: .5,
+                ),
                 itemCount: list.length,
                 itemBuilder: (context, index) {
                   CharacterModel character = list[index];
-                  return ListTile(
-                    title: Text(character.name),
-                    leading: Image.network(
-                      "${character.thumbnail.path}.${character.thumbnail.extension}",
+                  return SizedBox(
+                    width: 180,
+                    child: InkWell(
+                      borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(3),
+                        topRight: Radius.circular(3),
+                      ),
+                      splashColor: Colors.red.withOpacity(.5),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          Expanded(
+                            child: Ink(
+                              decoration: BoxDecoration(
+                                color: Colors.white24,
+                                borderRadius: const BorderRadius.only(
+                                  topLeft: Radius.circular(3),
+                                  topRight: Radius.circular(3),
+                                ),
+                                image: DecorationImage(
+                                  image: NetworkImage(
+                                      "${character.thumbnail.path}.${character.thumbnail.extension}"),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 8, horizontal: 5),
+                            height: 70,
+                            decoration: const BoxDecoration(
+                              color: Colors.white10,
+                              borderRadius: BorderRadius.only(
+                                bottomLeft: Radius.circular(3),
+                                bottomRight: Radius.circular(3),
+                              ),
+                            ),
+                            child: Text(
+                              character.name,
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                              softWrap: true,
+                              maxLines: 3,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          )
+                        ],
+                      ),
+                      onTap: () {},
                     ),
                   );
                 },
